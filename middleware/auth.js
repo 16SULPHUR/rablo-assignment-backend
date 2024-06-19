71a92789a57bc1eddd091b46a6876c541cfef087
“Ankit <22amtics124@gmail.com>
2024-06-19 15:15:01 +0530

    implemented authorization on endpoints

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,19 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
-  const token = req.header('Authorization')?.replace('Bearer ', '');
+  const token = req.header('Authorization');
+
   if (!token) {
-    return res.status(401).json({ message: 'Access denied, no token provided' });
+    return res.status(401).json({ message: 'Login to access products.' });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
     req.user = decoded;
     next();
-  } catch (error) {
-    res.status(400).json({ message: 'Invalid token' });
+  } catch (e) {
+    res.status(401).json({ message: 'Invalid token.' });
   }
 };
 
--- routes/product.js
+++ routes/product.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const { addProduct, getAllProducts, updateProduct, deleteProduct, getFeaturedProducts, getProductsByPrice, getProductsByRating } = require('../controllers/productController');
+const authMiddleware = require('../middleware/auth');
 
 const router = express.Router();
 
-router.post('/add', addProduct);
-router.get('/', getAllProducts);
-router.put('/:id', updateProduct);
-router.delete('/:id', deleteProduct);
-router.get('/featured', getFeaturedProducts);
-router.get('/price/:price', getProductsByPrice);
-router.get('/rating/:rating', getProductsByRating);
+router.post('/add',authMiddleware, addProduct);
+router.get('/',authMiddleware, getAllProducts);
+router.put('/:id',authMiddleware, updateProduct);
+router.delete('/:id',authMiddleware, deleteProduct);
+router.get('/featured',authMiddleware, getFeaturedProducts);
+router.get('/price/:price',authMiddleware, getProductsByPrice);
+router.get('/rating/:rating',authMiddleware, getProductsByRating);
 
 module.exports = router;
